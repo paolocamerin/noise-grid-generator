5f11802793c2f17e05f0f4267247704b196b1abf
paolocamerin <paolo92camerin@gmail.com>
2023-05-03 23:41:45 +0200

    Add credits and improve interface

--- src/code.ts
+++ src/code.ts
@@ -7,7 +7,7 @@
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-figma.ui.resize(300, 650);
+figma.ui.resize(300, 630);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
